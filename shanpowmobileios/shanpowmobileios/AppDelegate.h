5259eca56a93b6272951b826dcde78addfe9e705
celtavonce <celtavonce@gmail.com>
2013-11-29 15:02:14 +0800

    First Commit

---
+++ shanpowmobileios/shanpowmobileios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shanpowmobileios
+//
+//  Created by 木一 on 13-11-29.
+//  Copyright (c) 2013年 木一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shanpowmobileios/shanpowmobileios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  shanpowmobileios
+//
+//  Created by 木一 on 13-11-29.
+//  Copyright (c) 2013年 木一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
